3270afd7d19ebf5ceb4d473b5bd24d504a53b76c
daya <daya.sekar@yahoo.com>
2018-03-14 22:31:33 +0800

    Added Test class

--- src/main/java/org/daya/rdp/hammingDistance/HammingDistance.java
+++ src/main/java/org/daya/rdp/hammingDistance/HammingDistance.java
@@ -1,4 +1,44 @@
 package org.daya.rdp.hammingDistance;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HammingDistance {
+
+    static Logger LOGGER = LoggerFactory.getLogger(HammingDistance.class);
+
+    int computeClosestDistance(String one, String two) {
+        char charOne[] = one.toCharArray();
+        char charTwo[] = two.toCharArray();
+        int difference = 0;
+        for (int i = 0; i < charOne.length; i++) {
+            if (charOne[i] != charTwo[i])
+                difference++;
+        }
+        return difference;
+    }
+
+    List<Integer> computeClosestDistance(List<String> list) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            int previousDifference = list.get(i).length();
+            int currentDifference = 0;
+
+            for (int j = i + 1; j < list.size(); j++) {
+                currentDifference = computeClosestDistance(list.get(i), list.get(j));
+
+                if (currentDifference < previousDifference) {
+                    previousDifference = currentDifference;
+                }
+                LOGGER.trace("{} {} {} {}", list.get(i), list.get(j), currentDifference, previousDifference);
+            }
+            result.add(previousDifference);
+        }
+        return result;
+    }
 }
+
--- src/test/java/org/daya/rdp/hammingDistance/HammingDistanceTest.java
+++ src/test/java/org/daya/rdp/hammingDistance/HammingDistanceTest.java
@@ -1,5 +1,54 @@
 package org.daya.rdp.hammingDistance;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
 public class HammingDistanceTest {
 
+    HammingDistance testObject = new HammingDistance();
+
+    @Test
+    public void computeClosestDistance_whenEqual() {
+        int toVerify = testObject.computeClosestDistance("ONE", "ONE");
+        assertThat(toVerify, equalTo(0));
+    }
+
+    @Test
+    public void computeClosestDistance_case1() {
+        List<String> list = new ArrayList<>();
+
+        list.add("CTCCATCACAC");
+        list.add("AATATCTACAT");
+        list.add("ACATTCTCCAT");
+        list.add("CCTCCCCACTC");
+
+        List<Integer> toVerify = testObject.computeClosestDistance(list);
+        assertThat(toVerify, hasItems(5, 4, 8, 11));
+    }
+
+    @Test
+    public void computeClosestDistance_case2() {
+        List<String> list = new ArrayList<>();
+
+        list.add("AACACCCTATA");
+        list.add("CTTCATCCACA");
+        list.add("TTTCAATTTTC");
+        list.add("ACAATCAAACC");
+        list.add("ATTCTACAACT");
+        list.add("ATTCCTTATTC");
+        list.add("ACTTCTCTATT");
+        list.add("TAAAACTCACC");
+        list.add("CTTTTCCCACC");
+        list.add("ACCTTTTCTCA");
+        list.add("TACCACTACTT");
+
+        List<Integer> toVerify = testObject.computeClosestDistance(list);
+        assertThat(toVerify, hasItems(5, 4, 4, 5, 5, 6, 7, 6, 7, 9, 11));
+    }
 }
