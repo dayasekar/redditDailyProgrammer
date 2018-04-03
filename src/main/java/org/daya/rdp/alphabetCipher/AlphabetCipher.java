fd06370c9c7b829a4d5cbaff22cdc7bc6a212913
daya <daya.sekar@yahoo.com>
2018-04-03 08:44:18 +0800

    Alphabet Cipher

--- src/main/java/org/daya/rdp/alphabetCipher/AlphabetCipher.java
+++ src/main/java/org/daya/rdp/alphabetCipher/AlphabetCipher.java
@@ -1,8 +1,28 @@
 package org.daya.rdp.alphabetCipher;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class AlphabetCipher {
 
     public String encrypt(String seed, String plainText) {
         return null;
+    }
+
+    Map<String, Map<String, String>> getCipherBlock(String seed) {
+        char[] alphabets = IntStream.rangeClosed('a', 'z').mapToObj(c -> "" + (char) c).collect(Collectors.joining()).toCharArray();
+        Map<String, Map<String, String>> cipherBlock = new HashMap<>();
+        for (int i = 0; i < alphabets.length; i++) {
+
+            Map<String, String> map = new HashMap<>();
+
+            for (int j = 0; j < alphabets.length; j++) {
+            }
+
+            cipherBlock.put(String.valueOf(alphabets[i]), map);
+        }
+        return cipherBlock;
     }
 }
